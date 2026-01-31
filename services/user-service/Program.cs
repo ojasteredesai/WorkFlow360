57a5dfdde9f2bd70a4001fa27e294eec035dbeec
ojas teredesai <ojasteredesai@gmail.com>
2026-02-01 00:28:05 +0530

    updates related to docker

--- services/user-service/Program.cs
+++ services/user-service/Program.cs
@@ -6,6 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<UserDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Configuration
+.AddJsonFile("appsettings.json", optional: false)
+.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+.AddEnvironmentVariables();
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
